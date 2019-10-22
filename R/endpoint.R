ce28ea2474fa983fc7362dfc5e0bb92e27805ee3
Romain LESUR <romain.lesur@gmail.com>
2019-10-22 16:45:15 +0200

    modif doc

--- R/endpoint.R
+++ R/endpoint.R
@@ -1,11 +1,13 @@
 #' Get Insee endpoint
 #'
-#' Cette fonction renvoie le point d'accès aux API de l'Insee.
+#' Cette fonction renvoie les points d'accès définis par
+#' [api.insee.fr](https://api.insee.fr).
 #'
-#' @return Un objet de classe `oauth_endpoint`.
+#' @return Un objet de classe [oauth_endpoint][httr::oauth_endpoint].
 #' @export
 #'
 #' @encoding UTF-8
+#' @keywords internal
 #' @examples
 #' insee_endpoint()
 insee_endpoint <- function() {
