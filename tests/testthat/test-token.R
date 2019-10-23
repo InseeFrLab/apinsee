54ce806707740a95a187659d42d4d0e9c7d8e23e
Romain Lesur <romain.lesur@gmail.com>
2019-10-24 00:49:13 +0200

    test more classes

--- tests/testthat/test-token.R
+++ tests/testthat/test-token.R
@@ -3,4 +3,5 @@ test_that("insee_token() returns an OAuth2.0 token", {
   app <- httr::oauth_app("Test", Sys.getenv("INSEE_APP_KEY"), Sys.getenv("INSEE_APP_SECRET"))
   token <- insee_token(app, cache = FALSE)
   expect_s3_class(token, "TokenInsee")
+  expect_s3_class(token, "Token2.0")
 })
